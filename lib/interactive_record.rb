52053e390a7b474d3572ce08a3b32b515c5cc3d8
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-28 21:52:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,7 +20,7 @@ class InteractiveRecord
     column_names.compact
   end
   
-  def initialize(attributes = {})
+  def initialize(attributes = { })
     attributes.each do |key, value|
       self.send("#{key}=", value)
     end
